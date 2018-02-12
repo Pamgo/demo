5b23f19a3f200a37a7fddcc6c7d7646a6afca2b9
pamgo <pamgogdpyc@163.com>
2018-02-12 22:30:59 +0800

    ThreadLocal

---
+++ src/main/java/com/example/concurrency/threadLocal/Profiler.java
@@ -0,0 +1,26 @@
+package com.example.concurrency.threadLocal;
+
+import com.example.concurrency.SleepUtil;
+
+public class Profiler {
+
+	private static final ThreadLocal<Long> TIME_THREADLOCAL = new ThreadLocal<Long>() {
+		protected Long initialValue() {
+			return System.currentTimeMillis();
+		};
+	};
+	
+	public static final void begin() {
+		TIME_THREADLOCAL.set(System.currentTimeMillis());
+	}
+	
+	public static final long end() {
+		return System.currentTimeMillis() - TIME_THREADLOCAL.get();
+	}
+	
+	public static void main(String[] args) {
+		Profiler.begin();
+		SleepUtil.second(5);
+		System.out.println("Cost:" + Profiler.end() + "mills");
+	}
+}
