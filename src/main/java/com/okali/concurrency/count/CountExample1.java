f57986fddd7609c054b3c3ba0fdf972171b9e4ea
pamgo <pamgogdpyc@163.com>
2018-03-18 14:38:27 +0800

    加入依赖包，学习高并发

---
+++ src/main/java/com/okali/concurrency/count/CountExample1.java
@@ -0,0 +1,54 @@
+package com.okali.concurrency.count;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+import org.apache.log4j.Logger;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 非线程安全
+ * @author OKali
+ *
+ */
+@Slf4j
+public class CountExample1 {
+	
+	private static final Logger LOG = Logger.getLogger(CountExample1.class);
+	
+	// 请求总数
+	public static int clientTotal = 5000;
+	// 同时并发执行的线程数
+	public static int threadTotal = 200;
+	
+	public static long count = 0;
+	
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		final Semaphore semaphore = new Semaphore(threadTotal); // 同时申请200个请求
+		for (int i = 0; i < clientTotal; i++) {
+			executorService.execute(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						semaphore.acquire();// 申请一个许可
+						add();
+						semaphore.release(); // 释放一个许可
+					} catch (InterruptedException e) {
+						LOG.error("exception", e);
+					} 
+				}
+			});
+		}
+		executorService.shutdown();
+		LOG.info("count:" + count);
+	}
+
+	private static void add() {
+		count++ ;
+	}
+	
+}
---
+++ src/main/java/com/okali/concurrency/count/MapExample.java
@@ -0,0 +1,55 @@
+package com.okali.concurrency.count;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+import org.apache.log4j.Logger;
+
+import com.google.common.collect.Maps;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 多线程并发情况下非线程安全
+ * @author OKali
+ *
+ */
+@Slf4j
+public class MapExample {
+	
+	private static final Logger LOG = Logger.getLogger(CountExample1.class);
+
+	private static Map<Integer, Integer> map = Maps.newHashMap();
+	
+	private static int threadNum = 200; // 同时并发执行的线程数
+	private static int clientNum = 5000;
+	
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		final Semaphore semaphore = new Semaphore(threadNum); // 同时申请200个请求
+		for (int index = 0; index < clientNum; index ++) {
+			final int threadNum = index;
+			executorService.execute(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						semaphore.acquire();
+						func(threadNum);
+						semaphore.release();
+					} catch (InterruptedException e) {
+						LOG.error("Execption", e);
+					}
+				}
+			});
+		}
+		executorService.shutdown();
+		LOG.info("size:" + map.size());
+	}
+
+	public static void func(int threadNum) {
+		map.put(threadNum, threadNum);
+	}
+}
